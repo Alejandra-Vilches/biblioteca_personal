1a83f525412947aa6e04df1a4820cdb8dac45de2
Alejandra <ale.vilches.v@gmail.com>
2022-05-06 13:03:01 -0400

    desafio guiado mi propia biblioteca

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "books", to: "books#index"
+  get "books/new", to:"books#new"
+  post "books", to:"books#create"
+  get "books/:id/edit", to: "books#edit", as: "books_edit"
+  get "books/:id", to: "books#show", as: "book"
+  patch "books/:id", to: "books#update" 
+  
+
+  root "books#index"
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
